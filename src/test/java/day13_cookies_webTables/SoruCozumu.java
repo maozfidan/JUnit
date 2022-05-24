0bba465946bfe2d0716bf2124f2b04729f7ee28d
M.Arif Özfidan <ozfidanarif@gmail.com>
2022-05-24 22:27:48 +0200

    24.05.2022

---
+++ src/test/java/day13_cookies_webTables/SoruCozumu.java
@@ -0,0 +1,40 @@
+package day13_cookies_webTables;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import utilities.TestBase;
+
+import java.util.List;
+
+public class SoruCozumu extends TestBase {
+
+
+    @Test
+    public void test01() {
+
+        girisYap();
+
+    List<WebElement> liste=driver.findElements(By.xpath("//thead//tr[1]//th"));
+
+        System.out.println(liste.size());
+
+
+    }
+
+    private void girisYap() {
+
+        driver.get("https://www.hotelmycamp.com");
+        driver.findElement(By.linkText("Log in")).click();
+        Actions actions = new Actions(driver);
+        WebElement username = driver.findElement(By.id("UserName"));
+        actions.click(username).
+                sendKeys("manager").
+                sendKeys(Keys.TAB).
+                sendKeys("Manager1!").
+                sendKeys(Keys.ENTER).
+                perform();
+    }
+}
