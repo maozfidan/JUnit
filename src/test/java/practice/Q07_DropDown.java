9c0261f81a800fbf343ea7d45cc7c2a85de3b93b
M.Arif Özfidan <ozfidanarif@gmail.com>
2022-06-13 22:19:45 +0200

    13.06.2022

--- src/test/java/practice/Q07_DropDown.java
+++ src/test/java/practice/Q07_DropDown.java
@@ -1,5 +1,7 @@
 package practice;
 
+import org.junit.Test;
+
 public class Q07_DropDown {
     // go to url : https://www.jqueryscript.net/demo/Drop-Down-Combo-Tree/
     //-->Task1 Find Dropdown on Multi Selection
@@ -8,6 +10,34 @@ public class Q07_DropDown {
     //-->Task4 choose dropdown elements and printout dropdown elements' name  until choise 6
 
 
+//   @Test
+//   public void test(){
+//       // go to url : https://www.jqueryscript.net/demo/Drop-Down-Combo-Tree/
+//       driver.get("https://www.jqueryscript.net/demo/Drop-Down-Combo-Tree/");
 
+//       //-->Task1 Find Dropdown on Multi Selection
+//       driver.findElement(By.xpath("//input[@id ='justAnInputBox']")).click();
 
+//       //-->Task2 Find  all Dropdown Elements on page
+//       List<WebElement> dropList = driver.findElements(By.xpath("//span[@class ='comboTreeItemTitle']"));
+
+//       //-->Task3 printout DropDown Elements' number
+//       System.out.println("dropListteki elementlerin sayisi :" +dropList.size()); //dropListteki elementlerin sayisi :45
+
+//       System.out.println("********");
+
+//       //-->Task4 choose dropdown elements and printout dropdown elements' name
+
+//       for (int i = 0; i < dropList.size(); i++) {
+//           String text = dropList.get(i).getText();
+//           System.out.println("text = " + text);
+
+//           if(text.equals("choice 6")){
+//               dropList.get(i).click();
+//               break;
+//           }
+//       }
+//   }
+
+//
 }
